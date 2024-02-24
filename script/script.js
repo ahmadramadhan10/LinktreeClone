7ec470a79242b251e5b3716a052eca41cba66a80
Ahmad Ramadhan <aaarama21@gmail.com>
2024-02-25 00:37:10 +0700

    97% sisa bug di fitur share email

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
-const url = document.URL;
+let url = null; // variabel global
 
-function share(event){
+function share(event, link){
+    event.stopPropagation();
     modal.style.transform = 'translate(-50%, -50%) scale(1)';
     overlay.style.opacity = '1';
     overlay.style.pointerEvents = 'all';
-    event.stopPropagation();
+    url = link;
 }
 
 function tutup() {
@@ -14,8 +15,16 @@ function tutup() {
     modal.style.transform = 'translate(-50%, -50%) scale(0)';
     overlay.style.opacity = '0';
     overlay.style.pointerEvents = 'none';
+    url = null;
+}
+
+function share_link(api,comment) {
+    let link = api + url + comment;
+    //alert(link);
+    window.open(link,'_blank');
 }
 
 overlay.addEventListener("click", () => {
+    url = null;
     tutup();
 });
